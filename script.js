433fefa934721ee567a0843f4976e7e033d6988f
Ajjansha <141652880+Ajjansha@users.noreply.github.com>
2026-02-03 23:29:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,16 +8,16 @@ let scrollLeft;
 slider.addEventListener('mousedown', (e) => {
   isDown = true;
   slider.classList.add('active');
-  startX = e.pageX - slider.offsetLeft;
+  startX = e.pageX;
   scrollLeft = slider.scrollLeft;
 });
 
-slider.addEventListener('mouseleave', () => {
+slider.addEventListener('mouseup', () => {
   isDown = false;
   slider.classList.remove('active');
 });
 
-slider.addEventListener('mouseup', () => {
+slider.addEventListener('mouseleave', () => {
   isDown = false;
   slider.classList.remove('active');
 });
@@ -26,7 +26,6 @@ slider.addEventListener('mousemove', (e) => {
   if (!isDown) return;
   e.preventDefault();
 
-  const x = e.pageX - slider.offsetLeft;
-  const walk = (x - startX) * 2; // scroll speed
+  const walk = (e.pageX - startX) * 2;
   slider.scrollLeft = scrollLeft - walk;
 });
